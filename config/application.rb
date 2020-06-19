cf4b2af264f2513c53d0e4f508edda8a2ebeca77
Colin Dean <git@cad.cx>
2020-06-18 22:40:07 -0400

    Moves CORS config to a separate initializer

--- config/application.rb
+++ config/application.rb
@@ -23,14 +23,5 @@ module Inviter
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins "codeandsupply.co", "www.codeandsupply.co"
-        resource "/invitations",
-          headers: :any,
-          methods: [:post],
-          max_age: 3600
-      end
-    end
   end
 end
---
+++ config/initializers/cors.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'codeandsupply.co', 'www.codeandsupply.co'
+    resource '/invitations',
+             headers: :any,
+             methods: [:post],
+             max_age: 3600
+  end
+end
+
+Rails.application.config.hosts << 'codeandsupply.co'
+Rails.application.config.hosts << 'www.codeandsupply.co'
