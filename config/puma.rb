0764d344c62425a4862041d0ab30dc7adfd37713
Colin Dean <git@cad.cx>
2020-06-18 23:37:32 -0400

    Move sidekiq launch into puma config from rackup

--- config/puma.rb
+++ config/puma.rb
@@ -2,12 +2,23 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-preload_app!
-
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
+preload_app!
+
+if ENV["RAILS_ENV"] == "production" || ENV["RACK_ENV"] == "production"
+  before_fork do
+      @sidekiq_pid ||= spawn('bundle exec sidekiq -t 25')
+  end
+  on_restart do
+    Sidekiq.redis.shutdown { |conn| conn.close }
+  end
 end
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+plugin :tmp_restart
